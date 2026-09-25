38e4387a48a1b9dc6e57d032df1d1a26feb157e7
adriantulabut5747 <adriantulabut5747@gmail.com>
2026-09-25 21:44:07 +0800

    Revise lesson content on variables and data types
    Updated student name and date, added detailed explanations of variables and data types, and provided examples.

--- module-2-python-basics/lesson-1-variables-datatypes.py
+++ module-2-python-basics/lesson-1-variables-datatypes.py
@@ -1,13 +1,36 @@
 """
 Module 2 — Lesson 1: Variables & Data Types
-Student: [your name]
-Date: [date]
+Student: [ADRIAN A TULABUT]
+Date: [9/25/26]
 
 ============================================
 WHAT IS THIS TOPIC? (explain it like you're
 teaching a friend who's never coded before)
 ============================================
-[write your own explanation here]
+[this topic is what we call reserved variables or special syntax, you may see this on general programming languages 
+and they mostly use the same thing (eg float, boolean etc etc) 
+first of all we have variable, this is basically giving a code a name, so you wont get confused when calling it, and it will be
+more easier to familiarize your code, for example:
+
+name = "adrian" 
+
+in this case i used the variable and named it "name" and i declared a string onto that variable named "adrian"
+
+now for the data type, its basically asigning a value to a variable, earlier i used double quotes on "adrian" and this means i 
+used the string data type, now lets use int, which is short for integer, this data type lets us use numerical numbers (eg '1' '6')
+notice how i used single colons between the numbers, (string uses double colons "adrian" and integers use single colons '67')
+
+for the float variable its basically the same as the string but this time it has decimal points (67.67) use this when working with 
+decimal points (for example creating a calculator, where decimals are important)
+
+next is boolean, this is a special one because unlike the others, this one lets us use only 2 choices, (true and false) 
+we normally use this when working with system who only have either on or off, or true or false
+for example (lights_on = false) (adrian_pogi = true)
+
+we also have more like data types like char, which lets us store a single text, or a single string
+for example (char initial 'A')
+
+]
 
 
 ============================================
@@ -29,7 +52,11 @@ Write at least one working example below that you
 came up with yourself — not copied from class.
 """
 
-# --- your code example goes here ---
+name = "Adrian"
+age = 12
+initial = 'A'
+handsome = True 
+height = 6.2
 
 
 """
@@ -37,11 +64,21 @@ came up with yourself — not copied from class.
 A MISTAKE I MADE (or one I want to avoid)
 ============================================
 [what's something confusing or easy to get wrong
-about this topic?]
+about this topic?
+
+declaring datatypes and variables are really easy as long as you remember the syntaxes of them, for example when using string
+remember to use double quotes and when using char, us single ones
+]
 
 
 ============================================
 HOW THIS CONNECTS TO SOMETHING ELSE
 ============================================
-[optional]
+[
+variables and datatypes are use everywhere, and you cant code without them, this is basically how coding works, naming a variable, 
+giving it a datatype, and giving it a value, for example creating a calculator,
+or for more complex projects like object oriented programming
+which on that case, you can use a variable for naming the function
+and calling the variable later on and it runs the function
+] 
 """
